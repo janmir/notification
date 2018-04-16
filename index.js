afc800542c3382fca3ce2f5b7a60a494cdac2251
Jan Miranda <paulzu100@gmail.com>
2018-04-17 08:36:17 +0900

    Some changes

--- index.js
+++ index.js
@@ -2,78 +2,34 @@ import m from "./mithril.js"
 import anime from './anime.js'
 import { setTimeout, setInterval } from "timers";
 import tippy from 'tippy.js'
+// import {OneSignal} from 'https://cdn.onesignal.com/sdks/OneSignalSDK.js'
 
 /*********************One signal initializations************************/
 var OneSignal = window.OneSignal || [];
 OneSignal.push(function() {
   OneSignal.init({
     appId: "87a38029-bda5-4a10-a4c9-697492b36d8f",
+    allowLocalhostAsSecureOrigin: true,
     autoRegister: false,
     notifyButton: {
         enable: true,
     },
     welcomeNotification: {
-      "title": "Notifications",
-      "message": "Thanks for subscribing!",
-    },
+       "title": "Notifications",
+       "message": "Thanks for subscribing!",
+    }
   });
-});
+  OneSignal.on("subscriptionChange", function(isSubscribed) {
+    console.log("subscriptionChange")
+    // If the user's subscription state changes during the page's session, update the button text
+    m.redraw()
+  });
 
-OneSignal.push(function() {
-  // If we're on an unsupported browser, do nothing
-  if (!OneSignal.isPushNotificationsSupported()) {
-      return;
-  }
-  //updateMangeWebPushSubscriptionButton(buttonSelector);
-  //OneSignal.on("subscriptionChange", function(isSubscribed) {
-      /* If the user's subscription state changes during the page's session, update the button text */
-      //updateMangeWebPushSubscriptionButton(buttonSelector);
-  //});
 });
 /*********************Functions************************/
 var fn = {
-  onManageWebPushSubscriptionButtonClicked: function (event) {
-    fn.getSubscriptionState().then(function(state) {
-        if (state.isPushEnabled) {
-            /* Subscribed, opt them out */
-            OneSignal.setSubscription(false);
-        } else {
-            if (state.isOptedOut) {
-                /* Opted out, opt them back in */
-                OneSignal.setSubscription(true);
-            } else {
-                /* Unsubscribed, subscribe them */
-                OneSignal.registerForPushNotifications();
-            }
-        }
-    });
-    event.preventDefault();
-  },
-  updateMangeWebPushSubscriptionButton: function (buttonSelector) {
-    var hideWhenSubscribed = false;
-    var subscribeText = "Subscribe to Notifications";
-    var unsubscribeText = "Unsubscribe from Notifications";
-
-    fn.getSubscriptionState().then(function(state) {
-        var buttonText = !state.isPushEnabled || state.isOptedOut ? subscribeText : unsubscribeText;
-
-        var element = document.querySelector(buttonSelector);
-        if (element === null) {
-            return;
-        }
-
-        element.removeEventListener('click', fn.onManageWebPushSubscriptionButtonClicked);
-        element.addEventListener('click', fn.onManageWebPushSubscriptionButtonClicked);
-        element.textContent = buttonText;
-
-        if (state.hideWhenSubscribed && state.isPushEnabled) {
-            element.style.display = "none";
-        } else {
-            element.style.display = "";
-        }
-    });
-  },
   getSubscriptionState: function() {
+    console.log("getSubscriptionState")
     return Promise.all([
       OneSignal.isPushNotificationsEnabled(),
       OneSignal.isOptedOut()
@@ -85,6 +41,16 @@ var fn = {
             isPushEnabled: isPushEnabled,
             isOptedOut: isOptedOut
         };
+    });
+  },
+  doUpdate: ()=>{
+    fn.getSubscriptionState().then(function(state) {
+      console.log("oninit:getSubscriptionState")
+      console.log(state)
+  
+      App.isSubscribed = state.isPushEnabled;
+      App.hasUnsubscribed = state.isOptedOut;
+      m.redraw();
     });
   }
 }
@@ -112,9 +78,25 @@ var menu = {
 }
 
 var message = {
+  message:"Let's get subscrib'n, Push the button meow(now)!",
+  oninit: (vnode)=>{
+    console.log("Init message:")
+  },
+  oncreate:(vnode)=>{
+    console.log("Oncreate")
+  },
+  onupdate(vnode){
+    console.log("onupdate")
+  },
   view: (vnode)=>{
+    console.log("Redraw message:")
+
+    if (App.isSubscribed){
+      message.message = "You are already subscribed, off you go..."
+    }
+
     return  m("div#message",[
-      "Let's get subscrib'n, Push the button meow(now)!"
+      message.message
     ]);
   }
 }
@@ -130,18 +112,67 @@ var purr = {
 }
 
 var button = {
-  do: ()=>{
-    alert("something");
+  buttonLabel:"Subscribe",
+  oninit: (vnode)=>{
+    console.log("Init button:")
   },
+  do: ()=>{
+    fn.getSubscriptionState().then(function(state) {
+      console.log("do:getSubscriptionState")
+      console.log(state)
+      if (state.isPushEnabled) {
+          /* Subscribed, opt them out */
+          OneSignal.setSubscription(false);
+      } else {
+          if (state.isOptedOut) {
+              /* Opted out, opt them back in */
+              OneSignal.setSubscription(true);
+          } else {
+              /* Unsubscribed, subscribe them */
+              OneSignal.registerForPushNotifications();
+          }
+      }
+
+      fn.doUpdate()
+    });
+  },
+  oncreate:(vnode)=>{
+    console.log("Oncreate")
+  },
+  onupdate(vnode){
+    console.log("onupdate")
+  },
+
   view: (vnode)=>{
-    return  m("div#subButton",{onclick: button.do}, ["Subscribe"]);
+    console.log("Redraw button:")
+
+    if (App.isSubscribed){
+      button.buttonLabel = "Unsubscribe"
+    }
+
+    return  m("div#subButton",{onclick: button.do}, [
+      button.buttonLabel
+    ]);
   }
 }
 
 var App = {
+  isSubscribed: false,
+  hasUnsubscribed: false,
+  oninit: (vnode)=>{
+    console.log("Init app:")
+    fn.doUpdate()
+  },
+  oncreate:(vnode)=>{
+    console.log("Oncreate")
+  },
+  onupdate(vnode){
+    console.log("onupdate")
+  },
+
   view: (vnode)=>{
-    console.log("Redraw:", vnode);
-    
+    console.log("Redraw App:")
+    console.log(App)
     return  m("div#container", [
       m(menu),
       m("div#app",
