e12f1505ba138f44f99c77e3412e455d11ae6c00
Jan Miranda <paulzu100@gmail.com>
2018-04-18 14:12:31 +0900

    Aesthetics

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ import m from "./mithril.js"
 import anime from './anime.js'
 import { setTimeout, setInterval } from "timers";
 import tippy from 'tippy.js'
-// import {OneSignal} from 'https://cdn.onesignal.com/sdks/OneSignalSDK.js'
-
-console.log("Load v2...")
 
 /*********************One signal initializations************************/
-var OneSignal = window.OneSignal || [];
-OneSignal.push(function() {
+var OneSignal = window.OneSignal// || [];
+/*OneSignal.push(()=>{
   OneSignal.init({
     appId: "87a38029-bda5-4a10-a4c9-697492b36d8f",
     autoRegister: false,
@@ -17,17 +14,47 @@ OneSignal.push(function() {
     },
     welcomeNotification: {
        "title": "Notifications",
-       "message": "Thanks for subscribing!",
+       "message": "Hey {{ user | default: 'there' }}, Thanks for subscribing!",
     }
   });
+});
+OneSignal.push(()=>{
   OneSignal.on("subscriptionChange", function(isSubscribed) {
     console.log("subscriptionChange")
     console.log("The user's subscription state is now:", isSubscribed);
     // If the user's subscription state changes during the page's session, update the button text
     fn.doUpdate()
   });
+});*/
 
+OneSignal.init({
+  appId: "87a38029-bda5-4a10-a4c9-697492b36d8f",
+  autoRegister: false,
+  notifyButton: {
+      enable: false,
+  },
+  welcomeNotification: {
+     "title": "Notifications",
+     "message": "Hey {{ user | default: 'there' }}, Thanks for subscribing!",
+  }
 });
+OneSignal.on("subscriptionChange", function(isSubscribed) {
+  console.log("SubscriptionChange")
+  console.log("The user's subscription state is now:", isSubscribed);
+  if(isSubscribed){
+    //User
+    if(App.user !== ""){
+      OneSignal.sendTags({
+        user: App.user,
+        realm: App.realm,
+      }, function(tagsSent) {
+        console.log("Tags for user was set.")
+      });            
+    }    
+  }
+  fn.doUpdate()
+});
+
 /*********************Functions************************/
 var fn = {
   getSubscriptionState: function() {
@@ -68,7 +95,17 @@ var fn = {
 
 /*********************Components************************/
 var menu = {
-  about: "This is about message",
+  about: "Landing page for notification subscription.",
+  credits: `
+  Cat Images<br/><i>http://iconka.com/en/</i><br/>
+  Cat Puns<br/><i>http://blog.spikepadley.com/post/72332709432/cat-puns</i><br/>
+  `,
+  help: `
+  Push the button!, 
+  by pushing it you subscribe to web push 
+  notifications from this webapp.
+  `,
+
   oncreate: (vnode)=>{
     tippy(".menu_item",{
       trigger: 'click',
@@ -81,8 +118,8 @@ var menu = {
   view: (vnode)=>{
     return  m("div#menu",[
       m(".menu_item",{title:menu.about},"About"),
-      m(".menu_item","Credits"),
-      m(".menu_item","Help?"),
+      m(".menu_item",{title:menu.credits},"Credits"),
+      m(".menu_item",{title:menu.help},"Help?"),
       //m(".menu_item","with❤️by ;p")
     ]);
   }
@@ -91,8 +128,6 @@ var menu = {
 var message = {
   message:"...",
   view: (vnode)=>{
-    console.log("Redraw message:")
-
     return  m("div#message",[
       message.message
     ]);
@@ -130,33 +165,52 @@ var button = {
           } else {
             console.log("SUBSCRIBE:REGISTER")
             /* Unsubscribed, subscribe them */
-              OneSignal.registerForPushNotifications();
+            OneSignal.registerForPushNotifications();
           }
       }
     });
   },
   view: (vnode)=>{
-    return  m("div#subButton",{onclick: button.do}, [
+    return  m("div#subButton",{onclick: button.do, className: (App.isSubscribed?"unsubButton":"subButton")}, [
       button.buttonLabel
     ]);
   }
 }
 
 var App = {
+  user: "",
+  realm: "odtr",
   isSubscribed: false,
   hasUnsubscribed: false,
 
   oninit: (vnode)=>{
+    //get paths
+    let deco = ""
+    
+    try {
+      let url = new URL(window.location.href);
+      deco = window.atob(url.searchParams.get("u") || "")
+    }catch(error) {}
+
+    if (deco !== ""){
+      deco = decodeURIComponent(deco)
+      App.user = deco;
+    }
+
     fn.doUpdate();
+
+    console.log(App)
   },
   view: (vnode)=>{
     console.log("Redraw App:")
+    console.log("-----App-----")
     console.log(App)
+    console.log("-------------")
     return  m("div#container", [
       m(menu),
       m("div#app",
         [
-          m("div#greetings","Purr-fect day to you, Hooman!"),
+          m("div#greetings","Purrfect Day, Hooman!"),
           m("div#purr_container",[
             m(purr),
             m("div#count", 1)
